a8bfeef756b33af78a607f1b5758919a3a707c90
yanruifeng <xlyanrui@sina.com>
2023-12-15 21:02:59 +0800

    fix: 登录后台功能基础

---
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -0,0 +1,43 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Employee;
+import com.itheima.reggie.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request,@RequestBody Employee employee){
+        //1.将页面中提交的密码进行md5加密处理
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+        //2.根据页面中提交的用户名username查询数据库
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+        //3.如果没有查询到就返回登录失败结果
+        if (emp == null) return R.error("登录失败");
+        //4. 密码比对，如果不匹配，也返回失败
+        if (!emp.getPassword().equals(password)) return R.error("登录失败");
+        //5.查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+        if(emp.getStatus()==0) return R.error("账号已禁用");
+        //6.登录成功，将员工id存入Session并返回登录成功结果
+        request.getSession().setAttribute("employee",emp.getId());
+        return R.success(emp);
+    }
+}
