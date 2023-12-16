fa27488be47a9741ec6d2c0c7e45e5a8e7b03a51
yanruifeng <xlyanrui@sina.com>
2023-12-16 23:05:20 +0800

    添加菜品分类功能

---
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -0,0 +1,21 @@
+package com.itheima.reggie.controller;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.service.CategoryService;
+import com.itheima.reggie.service.DishFlavorService;
+import com.itheima.reggie.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/dish")
+public class DishController {
+    @Autowired
+    private DishService dishService;
+    @Autowired
+    private DishFlavorService dishFlavorService;
+}
