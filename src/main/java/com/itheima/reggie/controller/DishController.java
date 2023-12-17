13a034f4e5fafb320825763d06e59c01398e0ac0
yanruifeng <xlyanrui@sina.com>
2023-12-17 10:31:42 +0800

    fix: 完善查询菜品分页查询

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -1,12 +1,22 @@
 package com.itheima.reggie.controller;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.entity.Category;
 import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.service.CategoryService;
 import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @RestController
@@ -16,9 +26,39 @@ public class DishController {
     private DishService dishService;
     @Autowired
     private DishFlavorService dishFlavorService;
+    @Autowired
+    private CategoryService categoryService;
     @PostMapping
     public R<String> save(@RequestBody DishDto dishDto){
         dishService.saveWithFlavor(dishDto);
         return R.success("菜品添加成功");
+    }
+    @GetMapping("/page")
+    public R<Page<DishDto>> query(int page,int pageSize,String name){
+        //构造分页构造器对象
+        Page<Dish> pageInfo = new Page<>(page,pageSize);
+        Page<DishDto> dishDtoPage = new Page<>();
+        //条件构造器
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        //添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name),Dish::getName,name);
+        //添加排序条件
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+        //执行分页查询
+        dishService.page(pageInfo,queryWrapper);
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo,dishDtoPage,"records");
+        log.info("数据拷贝{}",dishDtoPage);
+
+        List<DishDto> list = pageInfo.getRecords().stream().map(item -> {
+            //根据id查询分类对象
+            DishDto dishDto = new DishDto();
+            BeanUtils.copyProperties(item, dishDto);
+            Category category = categoryService.getById(item.getCategoryId());
+            if (category != null) dishDto.setCategoryName(category.getName());
+            return dishDto;
+        }).collect(Collectors.toList());
+        dishDtoPage.setRecords(list);
+        return R.success(dishDtoPage);
     }
 }
